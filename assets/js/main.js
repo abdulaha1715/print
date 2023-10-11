38f4dc12c859c04c99c4b1f6e2ef35a821e1c986
Abdulaha Islam <63844021+abdulaha1715@users.noreply.github.com>
2023-10-11 15:44:36 +0600

    commit

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+/**
+ * My functions:
+ * #1 - Dropdown Arrow
+ * #2 - Responsive search box
+ * #3 - Responsive menu trigger
+ * #4 - Responsive menu close
+ * #5 - Home-Hero Area Slider
+ * #6 - Home-Carousel Most View Product
+ * #7 - Home-Payment Area Slider
+ * #8 - Cart-Quantity plus minus
+ * 
+ */
+
+;(function($) {
+    'use strict';
+
+    jQuery(document).ready(function($) {
+
+        $(".see-more-btn").click(function(){
+          $(".more-content").toggleClass("block");
+        });
+
+    });
+
+}(jQuery));
+
